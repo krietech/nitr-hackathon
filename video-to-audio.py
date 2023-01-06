a7a6b95a3e2729d588ebd293bf9d3df2aac22ca0
Mahesh N D <98675386+MaheshND-321@users.noreply.github.com>
2023-01-06 22:49:21 +0530

    video to audio convertion file

---
+++ video-to-audio.py
@@ -0,0 +1,24 @@
+# Author : Arnab Basak
+
+import os
+import imageio
+import moviepy.editor
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+Tk().withdraw()
+filelocation = askopenfilename()
+
+print('Conversion in progress...')
+
+basename = os.path.basename(filelocation)
+
+filename = os.path.splitext(basename)[0]
+
+video = moviepy.editor.VideoFileClip(filelocation)
+audio = video.audio
+
+aud = audio.write_audiofile(filename+".mp3")
+print(aud)
+
+print('Conversion Done Successfully!')
